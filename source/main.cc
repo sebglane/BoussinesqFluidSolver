8e2bd8bf95f6ad4cf3cac58031c32ffbd63abc13
sglane <glane@tu-berlin.de>
2018-11-07 12:13:26 -0800

    cmake works for simple program

---
+++ source/main.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "StringHandler.h"
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        std::cout << "Hello world." << std::endl;
+
+        const std::string dummy("Hello Mama");
+        StringHandler   my_string(dummy);
+        my_string.print(std::cout);
+    }
+    catch (std::exception &exc)
+    {
+        std::cerr << std::endl << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        std::cerr << "Exception on processing: " << std::endl
+                  << exc.what() << std::endl
+                  << "Aborting!" << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << std::endl << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        std::cerr << "Unknown exception!" << std::endl
+                  << "Aborting!" << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        return 1;
+    }
+    return 0;
+}
