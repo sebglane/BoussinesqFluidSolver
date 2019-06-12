15815a6ad5d60c41e8d4f7ec95e540a0ef84d11a
Ludwig Sebastian Glane <glane@tu-berlin.de>
2019-06-12 13:58:23 +0200

    fixed computation of output of spherical components, added vorticity

--- source/postprocessor.cc
+++ source/postprocessor.cc
@@ -20,7 +20,9 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
 {
     std::vector<std::string> solution_names(dim, "velocity");
     solution_names.emplace_back("pressure");
+
     solution_names.emplace_back("temperature");
+
     solution_names.emplace_back("partition");
 
     solution_names.emplace_back("radial_velocity");
@@ -37,6 +39,19 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
             Assert(false, ExcDimensionMismatch2(dim, 2, 3));
             break;
     }
+    switch (dim)
+    {
+        case 2:
+            solution_names.emplace_back("vorticity");
+            break;
+        case 3:
+            for (unsigned int d=0; d<3; ++d)
+                solution_names.emplace_back("vorticity");
+            break;
+        default:
+            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            break;
+    }
 
     return solution_names;
 }
@@ -44,7 +59,7 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
 template<int dim>
 UpdateFlags PostProcessor<dim>::get_needed_update_flags() const
 {
-    return update_values|update_quadrature_points;
+    return update_values|update_gradients|update_quadrature_points;
 }
 
 template<int dim>
@@ -52,21 +67,44 @@ std::vector<DataComponentInterpretation::DataComponentInterpretation>
 PostProcessor<dim>::get_data_component_interpretation() const
 {
     std::vector<DataComponentInterpretation::DataComponentInterpretation>
+
+    // velocity
     component_interpretation(dim, DataComponentInterpretation::component_is_part_of_vector);
+    // pressure
     component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+    // temperature
     component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+    // mpi partition
     component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
 
+    // radial velocity
     component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+    switch (dim)
+    {
+        case 2:
+            // azimuthal velocity
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            break;
+        case 3:
+            // azimuthal velocity
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            // polar velocity
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            break;
+        default:
+            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            break;
+    }
 
+    // vorticity
     switch (dim)
     {
         case 2:
             component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
             break;
         case 3:
-            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
-            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);            break;
+            component_interpretation.push_back(DataComponentInterpretation::component_is_part_of_vector);
+            break;
         default:
             Assert(false, ExcDimensionMismatch2(dim, 2, 3));
             break;
@@ -86,9 +124,9 @@ void PostProcessor<2>::evaluate_vector_field(
     Assert(computed_quantities.size() == n_quadrature_points,
            ExcDimensionMismatch(computed_quantities.size(),
                                 n_quadrature_points));
-    Assert(computed_quantities[0].size() == 2 * dim + 3,
+    Assert(computed_quantities[0].size() == 2 * dim + 4,
            ExcDimensionMismatch(computed_quantities[0].size(),
-                                2 * dim + 3));
+                                2 * dim + 4));
     Assert(inputs.solution_values[0].size() == dim + 2,
            ExcDimensionMismatch(inputs.solution_values[0].size(),
                                 dim + 2));
@@ -115,11 +153,26 @@ void PostProcessor<2>::evaluate_vector_field(
         const Tensor<1,dim>   azimuthal_basis_vector({-sin(phi), cos(phi)});
 
         // compute projection
+        double velocity_magnitude = 0;
+        computed_quantities[q](dim+3) = 0;
+        computed_quantities[q](dim+4) = 0;
         for (unsigned int d=0; d<dim; ++d)
         {
             computed_quantities[q](dim+3) += radial_basis_vector[d] * inputs.solution_values[q](d);
             computed_quantities[q](dim+4) += azimuthal_basis_vector[d] * inputs.solution_values[q](d);
+            velocity_magnitude += inputs.solution_values[q](d) * inputs.solution_values[q](d);
         }
+        velocity_magnitude = sqrt(velocity_magnitude);
+
+        Assert(computed_quantities[q](dim+3) <= velocity_magnitude,
+               ExcLowerRangeType<double>(computed_quantities[q](dim+3),
+                                         velocity_magnitude));
+        Assert(computed_quantities[q](dim+4) <= velocity_magnitude,
+               ExcLowerRangeType<double>(computed_quantities[q](dim+4),
+                                         velocity_magnitude));
+        // vorticity
+        computed_quantities[q](2*dim+3) = inputs.solution_gradients[q][1][0]
+                                      - inputs.solution_gradients[q][0][1];
     }
 }
 
@@ -134,9 +187,9 @@ void PostProcessor<3>::evaluate_vector_field(
     Assert(computed_quantities.size() == n_quadrature_points,
            ExcDimensionMismatch(computed_quantities.size(),
                                 n_quadrature_points));
-    Assert(computed_quantities[0].size() == 2 * dim + 3,
+    Assert(computed_quantities[0].size() == 3 * dim + 3,
            ExcDimensionMismatch(computed_quantities[0].size(),
-                                2 * dim + 3));
+                                3 * dim + 3));
     Assert(inputs.solution_values[0].size() == dim + 2,
            ExcDimensionMismatch(inputs.solution_values[0].size(),
                                 dim + 2));
@@ -167,13 +220,37 @@ void PostProcessor<3>::evaluate_vector_field(
         const Tensor<1,dim>   polar_basis_vector({cos(phi) * cos(theta), sin(phi) * cos(theta), sin(theta)});
         const Tensor<1,dim>   azimuthal_basis_vector({-sin(phi), cos(phi) , 0.});
 
-        // compute projection
+        // compute projection of spherical components
+        double velocity_magnitude = 0;
+        computed_quantities[q](dim+3) = 0;
+        computed_quantities[q](dim+4) = 0;
+        computed_quantities[q](dim+5) = 0;
         for (unsigned int d=0; d<dim; ++d)
         {
             computed_quantities[q](dim+3) += radial_basis_vector[d] * inputs.solution_values[q](d);
             computed_quantities[q](dim+4) += polar_basis_vector[d] * inputs.solution_values[q](d);
             computed_quantities[q](dim+5) += azimuthal_basis_vector[d] * inputs.solution_values[q](d);
+            velocity_magnitude += inputs.solution_values[q](d) * inputs.solution_values[q](d);
         }
+        velocity_magnitude = sqrt(velocity_magnitude);
+
+        Assert(computed_quantities[q](dim+3) <= velocity_magnitude,
+               ExcLowerRangeType<double>(computed_quantities[q](dim+3),
+                                         velocity_magnitude));
+        Assert(computed_quantities[q](dim+4) <= velocity_magnitude,
+               ExcLowerRangeType<double>(computed_quantities[q](dim+4),
+                                         velocity_magnitude));
+        Assert(computed_quantities[q](dim+5) <= velocity_magnitude,
+               ExcLowerRangeType<double>(computed_quantities[q](dim+5),
+                                         velocity_magnitude));
+
+        // vorticity
+        computed_quantities[q](dim+6) = inputs.solution_gradients[q][2][1]
+                                      - inputs.solution_gradients[q][1][2];
+        computed_quantities[q](dim+7) = inputs.solution_gradients[q][0][2]
+                                      - inputs.solution_gradients[q][2][0];
+        computed_quantities[q](dim+8) = inputs.solution_gradients[q][1][0]
+                                      - inputs.solution_gradients[q][0][1];
     }
 }
 
