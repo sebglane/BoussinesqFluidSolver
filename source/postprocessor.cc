8529b1b16e35c138d4ec8a242204f7736ec53d9f
Ludwig Sebastian Glane <glane@tu-berlin.de>
2019-06-13 14:22:12 +0200

    added temperature gradient to output variables

--- source/postprocessor.cc
+++ source/postprocessor.cc
@@ -53,6 +53,24 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
             break;
     }
 
+    for (unsigned int d=0; d<dim; ++d)
+        solution_names.emplace_back("temperature_gradient");
+
+    solution_names.emplace_back("radial_temperature_gradient");
+    switch (dim)
+    {
+        case 2:
+            solution_names.emplace_back("azimuthal_temperature_gradient");
+            break;
+        case 3:
+            solution_names.emplace_back("polar_temperature_gradient");
+            solution_names.emplace_back("azimuthal_temperature_gradient");
+            break;
+        default:
+            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            break;
+    }
+
     return solution_names;
 }
 
@@ -110,6 +128,29 @@ PostProcessor<dim>::get_data_component_interpretation() const
             break;
     }
 
+    // temperature gradient
+    for (unsigned int d=0; d<dim; ++d)
+        component_interpretation.push_back(DataComponentInterpretation::component_is_part_of_vector);
+
+    // spherical components of temperature gradient
+    component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+    switch (dim)
+    {
+        case 2:
+            // azimuthal temperature gradient
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            break;
+        case 3:
+            // azimuthal temperature gradient
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            // polar temperature gradient
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            break;
+        default:
+            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            break;
+    }
+
     return component_interpretation;
 }
 
@@ -124,24 +165,17 @@ void PostProcessor<2>::evaluate_vector_field(
     Assert(computed_quantities.size() == n_quadrature_points,
            ExcDimensionMismatch(computed_quantities.size(),
                                 n_quadrature_points));
-    Assert(computed_quantities[0].size() == 2 * dim + 4,
+    Assert(computed_quantities[0].size() == 4 * dim + 4,
            ExcDimensionMismatch(computed_quantities[0].size(),
-                                2 * dim + 4));
+                                4 * dim + 4));
     Assert(inputs.solution_values[0].size() == dim + 2,
            ExcDimensionMismatch(inputs.solution_values[0].size(),
                                 dim + 2));
+    Assert(inputs.solution_gradients[0].size() == dim + 2,
+           ExcDimensionMismatch(inputs.solution_gradients[0].size(),
+                                dim + 2));
     for (unsigned int q=0; q<n_quadrature_points; ++q)
     {
-        // velocity
-        for (unsigned int d=0; d<dim; ++d)
-            computed_quantities[q](d) = inputs.solution_values[q](d);
-        // pressure
-        computed_quantities[q](dim) = inputs.solution_values[q](dim);
-        // temperature
-        computed_quantities[q](dim+1) = inputs.solution_values[q](dim+1);
-        // mpi partition
-        computed_quantities[q](dim+2) = partition;
-
         // spherical coordinates
         const Point<dim>    point = inputs.evaluation_points[q];
         const double phi = atan2(point[1], point[0]);
@@ -152,7 +186,17 @@ void PostProcessor<2>::evaluate_vector_field(
         const Tensor<1,dim>   radial_basis_vector({cos(phi) , sin(phi)});
         const Tensor<1,dim>   azimuthal_basis_vector({-sin(phi), cos(phi)});
 
-        // compute projection
+        // velocity
+        for (unsigned int d=0; d<dim; ++d)
+            computed_quantities[q](d) = inputs.solution_values[q](d);
+        // pressure
+        computed_quantities[q](dim) = inputs.solution_values[q](dim);
+        // temperature
+        computed_quantities[q](dim+1) = inputs.solution_values[q](dim+1);
+        // mpi partition
+        computed_quantities[q](dim+2) = partition;
+
+        // spherical components of velocity
         double velocity_magnitude = 0;
         computed_quantities[q](dim+3) = 0;
         computed_quantities[q](dim+4) = 0;
@@ -173,6 +217,29 @@ void PostProcessor<2>::evaluate_vector_field(
         // vorticity
         computed_quantities[q](2*dim+3) = inputs.solution_gradients[q][1][0]
                                       - inputs.solution_gradients[q][0][1];
+
+        // temperature gradient
+        for (unsigned int d=0; d<dim; ++d)
+            computed_quantities[q](2*dim+4+d) = inputs.solution_gradients[q][dim+1][d];
+
+        // spherical components of temperature gradient
+        double temperature_gradient_magnitude = 0;
+        computed_quantities[q](3*dim+4) = 0;
+        computed_quantities[q](3*dim+5) = 0;
+        for (unsigned int d=0; d<dim; ++d)
+        {
+            computed_quantities[q](3*dim+4) += radial_basis_vector[d] * inputs.solution_gradients[q][dim+1][d];
+            computed_quantities[q](3*dim+5) += azimuthal_basis_vector[d] * inputs.solution_gradients[q][dim+1][d];
+            temperature_gradient_magnitude += inputs.solution_gradients[q][dim+1][d] * inputs.solution_gradients[q][dim+1][d];
+        }
+        temperature_gradient_magnitude = sqrt(temperature_gradient_magnitude);
+
+        Assert(computed_quantities[q](3*dim+4) <= temperature_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](3*dim+4),
+                                         temperature_gradient_magnitude));
+        Assert(computed_quantities[q](3*dim+5) <= temperature_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](3*dim+5),
+                                         temperature_gradient_magnitude));
     }
 }
 
@@ -189,22 +256,12 @@ void PostProcessor<3>::evaluate_vector_field(
                                 n_quadrature_points));
     Assert(computed_quantities[0].size() == 3 * dim + 3,
            ExcDimensionMismatch(computed_quantities[0].size(),
-                                3 * dim + 3));
+                                5 * dim + 3));
     Assert(inputs.solution_values[0].size() == dim + 2,
            ExcDimensionMismatch(inputs.solution_values[0].size(),
                                 dim + 2));
     for (unsigned int q=0; q<n_quadrature_points; ++q)
     {
-        // velocity
-        for (unsigned int d=0; d<dim; ++d)
-            computed_quantities[q](d) = inputs.solution_values[q](d);
-        // pressure
-        computed_quantities[q](dim) = inputs.solution_values[q](dim);
-        // temperature
-        computed_quantities[q](dim+1) = inputs.solution_values[q](dim+1);
-        // mpi partition
-        computed_quantities[q](dim+2) = partition;
-
         // spherical coordinates
         const Point<dim>    point = inputs.evaluation_points[q];
         const double cylinder_radius = sqrt(point[0]*point[0] + point[1]*point[1]);
@@ -220,7 +277,17 @@ void PostProcessor<3>::evaluate_vector_field(
         const Tensor<1,dim>   polar_basis_vector({cos(phi) * cos(theta), sin(phi) * cos(theta), sin(theta)});
         const Tensor<1,dim>   azimuthal_basis_vector({-sin(phi), cos(phi) , 0.});
 
-        // compute projection of spherical components
+        // velocity
+        for (unsigned int d=0; d<dim; ++d)
+            computed_quantities[q](d) = inputs.solution_values[q](d);
+        // pressure
+        computed_quantities[q](dim) = inputs.solution_values[q](dim);
+        // temperature
+        computed_quantities[q](dim+1) = inputs.solution_values[q](dim+1);
+        // mpi partition
+        computed_quantities[q](dim+2) = partition;
+
+        // spherical components of velocity
         double velocity_magnitude = 0;
         computed_quantities[q](dim+3) = 0;
         computed_quantities[q](dim+4) = 0;
@@ -245,12 +312,40 @@ void PostProcessor<3>::evaluate_vector_field(
                                          velocity_magnitude));
 
         // vorticity
-        computed_quantities[q](dim+6) = inputs.solution_gradients[q][2][1]
-                                      - inputs.solution_gradients[q][1][2];
-        computed_quantities[q](dim+7) = inputs.solution_gradients[q][0][2]
-                                      - inputs.solution_gradients[q][2][0];
-        computed_quantities[q](dim+8) = inputs.solution_gradients[q][1][0]
-                                      - inputs.solution_gradients[q][0][1];
+        computed_quantities[q](2*dim+3) = inputs.solution_gradients[q][2][1]
+                                        - inputs.solution_gradients[q][1][2];
+        computed_quantities[q](2*dim+4) = inputs.solution_gradients[q][0][2]
+                                        - inputs.solution_gradients[q][2][0];
+        computed_quantities[q](2*dim+5) = inputs.solution_gradients[q][1][0]
+                                        - inputs.solution_gradients[q][0][1];
+
+        // temperature gradient
+        for (unsigned int d=0; d<dim; ++d)
+            computed_quantities[q](3*dim+3+d) = inputs.solution_gradients[q][dim+1][d];
+
+        // spherical components of temperature gradient
+        double temperature_gradient_magnitude = 0;
+        computed_quantities[q](4*dim+3) = 0;
+        computed_quantities[q](4*dim+4) = 0;
+        computed_quantities[q](4*dim+5) = 0;
+        for (unsigned int d=0; d<dim; ++d)
+        {
+            computed_quantities[q](4*dim+3) += radial_basis_vector[d] * inputs.solution_gradients[q][dim+1][d];
+            computed_quantities[q](4*dim+4) += azimuthal_basis_vector[d] * inputs.solution_gradients[q][dim+1][d];
+            computed_quantities[q](4*dim+5) += polar_basis_vector[d] * inputs.solution_gradients[q][dim+1][d];
+            temperature_gradient_magnitude += inputs.solution_gradients[q][dim+1][d] * inputs.solution_gradients[q][dim+1][d];
+        }
+        temperature_gradient_magnitude = sqrt(temperature_gradient_magnitude);
+
+        Assert(computed_quantities[q](4*dim+3) <= temperature_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](4*dim+3),
+                                         temperature_gradient_magnitude));
+        Assert(computed_quantities[q](4*dim+4) <= temperature_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](4*dim+4),
+                                         temperature_gradient_magnitude));
+        Assert(computed_quantities[q](4*dim+5) <= temperature_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](4*dim+5),
+                                         temperature_gradient_magnitude));
     }
 }
 
