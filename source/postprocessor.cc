33eae824c91db180414dc3313aa79b1e2bb52bb4
Ludwig Sebastian Glane <glane@tu-berlin.de>
2019-07-04 12:28:17 +0200

    added pressure gradient to vtk output

--- source/postprocessor.cc
+++ source/postprocessor.cc
@@ -36,7 +36,7 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
             solution_names.emplace_back("azimuthal_velocity");
             break;
         default:
-            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            Assert(false, ExcImpossibleInDim(dim));
             break;
     }
     switch (dim)
@@ -49,7 +49,7 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
                 solution_names.emplace_back("vorticity");
             break;
         default:
-            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            Assert(false, ExcImpossibleInDim(dim));
             break;
     }
 
@@ -67,12 +67,30 @@ std::vector<std::string> PostProcessor<dim>::get_names() const
             solution_names.emplace_back("azimuthal_temperature_gradient");
             break;
         default:
-            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            Assert(false, ExcImpossibleInDim(dim));
+            break;
+    }
+
+    for (unsigned int d=0; d<dim; ++d)
+        solution_names.emplace_back("pressure_gradient");
+
+    solution_names.emplace_back("radial_pressure_gradient");
+    switch (dim)
+    {
+        case 2:
+            solution_names.emplace_back("azimuthal_pressure_gradient");
+            break;
+        case 3:
+            solution_names.emplace_back("polar_pressure_gradient");
+            solution_names.emplace_back("azimuthal_pressure_gradient");
+            break;
+        default:
+            Assert(false, ExcImpossibleInDim(dim));
             break;
     }
 
     AssertDimension(solution_names.size(),
-                    (dim==2? 4 * dim + 4: 5 * dim + 3))
+                    (dim==2? 6 * dim + 4: 7 * dim + 3))
 
     return solution_names;
 }
@@ -113,7 +131,7 @@ PostProcessor<dim>::get_data_component_interpretation() const
             component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
             break;
         default:
-            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            Assert(false, ExcImpossibleInDim(dim));
             break;
     }
 
@@ -128,7 +146,7 @@ PostProcessor<dim>::get_data_component_interpretation() const
                 component_interpretation.push_back(DataComponentInterpretation::component_is_part_of_vector);
             break;
         default:
-            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            Assert(false, ExcImpossibleInDim(dim));
             break;
     }
 
@@ -151,12 +169,35 @@ PostProcessor<dim>::get_data_component_interpretation() const
             component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
             break;
         default:
-            Assert(false, ExcDimensionMismatch2(dim, 2, 3));
+            Assert(false, ExcImpossibleInDim(dim));
+            break;
+    }
+
+    // pressure gradient
+    for (unsigned int d=0; d<dim; ++d)
+        component_interpretation.push_back(DataComponentInterpretation::component_is_part_of_vector);
+
+    // spherical components of pressure gradient
+    component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+    switch (dim)
+    {
+        case 2:
+            // azimuthal pressure gradient
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            break;
+        case 3:
+            // azimuthal pressure gradient
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            // polar pressure gradient
+            component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+            break;
+        default:
+            Assert(false, ExcImpossibleInDim(dim));
             break;
     }
 
     AssertDimension(component_interpretation.size(),
-                    (dim==2? 4 * dim + 4: 5 * dim + 3))
+                    (dim==2? 6 * dim + 4: 7 * dim + 3))
 
     return component_interpretation;
 }
@@ -172,9 +213,9 @@ void PostProcessor<2>::evaluate_vector_field(
     Assert(computed_quantities.size() == n_quadrature_points,
            ExcDimensionMismatch(computed_quantities.size(),
                                 n_quadrature_points));
-    Assert(computed_quantities[0].size() == 4 * dim + 4,
+    Assert(computed_quantities[0].size() == 6 * dim + 4,
            ExcDimensionMismatch(computed_quantities[0].size(),
-                                4 * dim + 4));
+                                6 * dim + 4));
     Assert(inputs.solution_values[0].size() == dim + 2,
            ExcDimensionMismatch(inputs.solution_values[0].size(),
                                 dim + 2));
@@ -247,6 +288,29 @@ void PostProcessor<2>::evaluate_vector_field(
         Assert(computed_quantities[q](3*dim+5) <= temperature_gradient_magnitude + 1e-12,
                ExcLowerRangeType<double>(computed_quantities[q](3*dim+5),
                                          temperature_gradient_magnitude));
+
+        // pressure gradient
+        for (unsigned int d=0; d<dim; ++d)
+            computed_quantities[q](4*dim+4+d) = inputs.solution_gradients[q][dim][d];
+
+        // spherical components of temperature gradient
+        double pressure_gradient_magnitude = 0;
+        computed_quantities[q](5*dim+4) = 0;
+        computed_quantities[q](5*dim+5) = 0;
+        for (unsigned int d=0; d<dim; ++d)
+        {
+            computed_quantities[q](5*dim+4) += radial_basis_vector[d] * inputs.solution_gradients[q][dim][d];
+            computed_quantities[q](5*dim+5) += azimuthal_basis_vector[d] * inputs.solution_gradients[q][dim][d];
+            pressure_gradient_magnitude += inputs.solution_gradients[q][dim][d] * inputs.solution_gradients[q][dim][d];
+        }
+        pressure_gradient_magnitude = sqrt(pressure_gradient_magnitude);
+
+        Assert(computed_quantities[q](5*dim+4) <= pressure_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](5*dim+4),
+                                         pressure_gradient_magnitude));
+        Assert(computed_quantities[q](5*dim+5) <= pressure_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](5*dim+5),
+                                         pressure_gradient_magnitude));
     }
 }
 
@@ -261,9 +325,9 @@ void PostProcessor<3>::evaluate_vector_field(
     Assert(computed_quantities.size() == n_quadrature_points,
            ExcDimensionMismatch(computed_quantities.size(),
                                 n_quadrature_points));
-    Assert(computed_quantities[0].size() == 5 * dim + 3,
+    Assert(computed_quantities[0].size() == 7 * dim + 3,
            ExcDimensionMismatch(computed_quantities[0].size(),
-                                5 * dim + 3));
+                                7 * dim + 3));
     Assert(inputs.solution_values[0].size() == dim + 2,
            ExcDimensionMismatch(inputs.solution_values[0].size(),
                                 dim + 2));
@@ -353,6 +417,34 @@ void PostProcessor<3>::evaluate_vector_field(
         Assert(computed_quantities[q](4*dim+5) <= temperature_gradient_magnitude + 1e-12,
                ExcLowerRangeType<double>(computed_quantities[q](4*dim+5),
                                          temperature_gradient_magnitude));
+
+        // pressure gradient
+        for (unsigned int d=0; d<dim; ++d)
+            computed_quantities[q](5*dim+3+d) = inputs.solution_gradients[q][dim][d];
+
+        // spherical components of pressure gradient
+        double pressure_gradient_magnitude = 0;
+        computed_quantities[q](6*dim+3) = 0;
+        computed_quantities[q](6*dim+4) = 0;
+        computed_quantities[q](6*dim+5) = 0;
+        for (unsigned int d=0; d<dim; ++d)
+        {
+            computed_quantities[q](6*dim+3) += radial_basis_vector[d] * inputs.solution_gradients[q][dim][d];
+            computed_quantities[q](6*dim+4) += azimuthal_basis_vector[d] * inputs.solution_gradients[q][dim][d];
+            computed_quantities[q](6*dim+5) += polar_basis_vector[d] * inputs.solution_gradients[q][dim][d];
+            pressure_gradient_magnitude += inputs.solution_gradients[q][dim][d] * inputs.solution_gradients[q][dim][d];
+        }
+        pressure_gradient_magnitude = sqrt(pressure_gradient_magnitude);
+
+        Assert(computed_quantities[q](6*dim+3) <= pressure_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](6*dim+3),
+                                         pressure_gradient_magnitude));
+        Assert(computed_quantities[q](6*dim+4) <= pressure_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](6*dim+4),
+                                         pressure_gradient_magnitude));
+        Assert(computed_quantities[q](6*dim+5) <= temperature_gradient_magnitude + 1e-12,
+               ExcLowerRangeType<double>(computed_quantities[q](4*dim+5),
+                                         pressure_gradient_magnitude));
     }
 }
 
