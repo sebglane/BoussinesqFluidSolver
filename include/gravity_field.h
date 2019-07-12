8de764ce8647a3e324f090aa8b770ab722092819
sebglane <glane@tu-berlin.de>
2019-07-12 08:49:24 +0200

    externalized gravity field and corrected linear profile

---
+++ include/gravity_field.h
@@ -0,0 +1,55 @@
+/*
+ * gravity_field.cc
+ *
+ *  Created on: Jul 11, 2019
+ *      Author: sg
+ */
+
+#ifndef INCLUDE_GRAVITY_FIELD_CC_
+#define INCLUDE_GRAVITY_FIELD_CC_
+
+#include <deal.II/base/tensor_function.h>
+
+#include <exceptions.h>
+
+namespace EquationData {
+
+/*
+ *
+ * enumeration for the type of the gravity profile
+ *
+ */
+enum GravityProfile
+{
+    Constant,
+    Linear,
+    default_profile=Linear
+};
+
+using namespace dealii;
+
+template <int dim>
+class GravityFunction : public TensorFunction<1,dim>
+{
+public:
+    GravityFunction(const double    outer_radius = 1.0,
+                    const GravityProfile    profile_type = GravityProfile::Constant);
+
+    virtual Tensor<1,dim>   value(const Point<dim> &p) const;
+
+    virtual void            value_list(const std::vector<Point<dim>>    &points,
+                                       std::vector<Tensor<1,dim>>       &values) const;
+
+    GravityProfile  get_profile_type() const;
+
+private:
+    const double outer_radius;
+
+    const double scaling_factor = 20. / 13.;
+
+    const GravityProfile    profile_type;
+};
+
+}  // namespace EquationData
+
+#endif /* INCLUDE_GRAVITY_FIELD_CC_ */
--- source/initial_values.cc
+++ source/initial_values.cc
@@ -216,61 +216,6 @@ double TemperatureInitialValues<3>::value(
     return value;
 }
 
-
-template<int dim>
-GravityFunction<dim>::GravityFunction
-(const double outer_radius,
- const GravityProfile  profile_type)
-:
-TensorFunction<1,dim>(),
-outer_radius(outer_radius),
-profile_type(profile_type)
-{}
-
-template<int dim>
-Tensor<1,dim> GravityFunction<dim>::value(const Point<dim> &point) const
-{
-    const double r = point.norm();
-    Assert(r > 0, ExcNegativeRadius(r));
-
-    Tensor<1,dim> value;
-
-    switch (profile_type)
-    {
-        case GravityProfile::Constant:
-        {
-            value = -point / r;
-            break;
-        }
-        case GravityProfile::Linear:
-        {
-            value = -point / outer_radius;
-            break;
-        }
-        default:
-            break;
-    }
-    return value;
-}
-
-template<int dim>
-void GravityFunction<dim>::value_list(const std::vector<Point<dim>>    &points,
-                                    std::vector<Tensor<1,dim>> &values) const
-{
-    Assert(points.size() == values.size(),
-           ExcDimensionMismatch(points.size(), values.size()));
-    for (unsigned int i=0; i<points.size(); ++i)
-        values[i] = value(points[i]);
-}
-
-template<int dim>
-GravityProfile GravityFunction<dim>::get_profile_type() const
-{
-    return profile_type;
-}
-
-
-
 }  // namespace EquationData
 
 // explicit instantiation
@@ -279,7 +224,3 @@ template class EquationData::VelocityTestValues<3>;
 
 template class EquationData::TemperatureInitialValues<2>;
 template class EquationData::TemperatureInitialValues<3>;
-
-template class EquationData::GravityFunction<1>;
-template class EquationData::GravityFunction<2>;
-template class EquationData::GravityFunction<3>;
