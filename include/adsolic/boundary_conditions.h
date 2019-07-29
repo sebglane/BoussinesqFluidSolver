712460e5d4d6f9476eecf0ad598908dba0f938e7
Ludwig Sebastian Glane <glane@tu-berlin.de>
2019-07-29 19:20:15 +0200

    added basic boundary conditions

--- include/adsolic/boundary_conditions.h
+++ include/adsolic/boundary_conditions.h
@@ -24,7 +24,7 @@ using namespace dealii;
 namespace BC
 {
 
-/*
+/**
  * Structure that keeps all information about boundary
  * conditions. Necessary to enable different classes to share the boundary
  * conditions.
@@ -56,6 +56,40 @@ struct ScalarBoundaryConditions
 
   std::array<std::pair<types::boundary_id,types::boundary_id>,dim>  periodic_bcs;
 };
+
+/**
+ * Structure that keeps all information about boundary
+ * conditions. Necessary to enable different classes to share the boundary
+ * conditions.
+ */
+template <int dim>
+struct NavierStokesBoundaryConditions
+{
+    NavierStokesBoundaryConditions();
+
+  void clear_all_boundary_conditions();
+
+  void set_dirichlet_bc(const types::boundary_id                boundary_id,
+                        const std::shared_ptr<Function<dim>>   &dirichlet_function
+                        = std::shared_ptr<Function<dim>>());
+
+  void set_neumann_bc(const types::boundary_id              boundary_id,
+                      const std::shared_ptr<Function<dim>> &neumann_function
+                      = std::shared_ptr<Function<dim>>());
+
+  void set_periodic_bc(const unsigned int       direction,
+                       const types::boundary_id first_boundary_id,
+                       const types::boundary_id second_boundary_id);
+
+  std::map<types::boundary_id,std::shared_ptr<Function<dim>>>   dirichlet_bcs;
+  std::map<types::boundary_id,std::shared_ptr<Function<dim>>>   neumann_bcs;
+
+  std::set<types::boundary_id>  dirichlet_ids;
+  std::set<types::boundary_id>  neumann_ids;
+
+  std::array<std::pair<types::boundary_id,types::boundary_id>,dim>  periodic_bcs;
+};
+
 
 }  // namespace BoundaryConditions
 
